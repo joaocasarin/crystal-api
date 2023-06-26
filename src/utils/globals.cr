e50cbc195ae480683ab71bc9d0a32e3d008ee14e
Joao Vitor Casarin <jcasarin@apple.com>
2023-06-26 00:52:59 -0300

    refactor: add missing Episode type alias

--- src/utils/globals.cr
+++ src/utils/globals.cr
@@ -1,5 +1,6 @@
 module GlobalTypes
-    alias Residents = Array(NamedTuple(id: Int32, episode: Array(NamedTuple(id: Int32))))
+    alias Episode = Array(NamedTuple(id: Int32))
+    alias Residents = Array(NamedTuple(id: Int32, episode: Episode))
     alias TravelStop = NamedTuple(id: Int32, name: String, type: String, dimension: String, residents: Residents)
     alias TravelStops = Array(TravelStop)
 end
